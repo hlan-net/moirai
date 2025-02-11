770fe6b6133ceace2d167a83e1d7df0a6dcc5d7d
Lari Korpi <Lari Korpi>
2025-02-11 14:50:18 +0200

    starting the FE edits

---
+++ ui/src/components/MainPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <h3>To Be Added</h3>
+    <ul>
+      <li>Status</li>
+      <li>Feeds</li>
+      <li>Events</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
