29174aef0f6ddee65569eaae275db353e85c3c10
Lari Korpi <Lari Korpi>
2025-02-13 10:04:17 +0200

    cleanup temp frontend

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  outputDir: 'dist',
-  publicPath: '/ui'
+  base: '/ui',
+  build: {
+    outDir: 'dist'
+  }
 })
