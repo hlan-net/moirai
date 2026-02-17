e86f58a309e6216a92fab79d8ff925608f692368
Lari Korpi <Lari Korpi>
2026-02-17 11:36:50 +0200

    test(ui): move back to headless playwright testing

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -2,13 +2,17 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 10000,
+  expect: {
+    timeout: 5000,
+  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: 'html',
   use: {
-    baseURL: (process.env.CI || process.env.TEST_TARGET === 'docker') ? 'http://localhost:8088' : 'http://localhost:5173',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || ((process.env.CI || process.env.TEST_TARGET === 'docker') ? 'http://localhost:8088' : 'http://localhost:5173'),
     trace: 'on-first-retry',
     httpCredentials: {
       username: process.env.API_USERNAME || 'testuser',
